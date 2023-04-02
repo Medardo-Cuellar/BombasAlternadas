7355b180065fbae4c1e62a7e093031642785ce70
Medardo Cuellar <5419101316@utnuevolaredo.edu.mx>
2023-04-02 05:19:49 -0500

    Se hace el mismo codigo pero para la raspberry pi pico

---
+++ BombasAlternadas.py
@@ -0,0 +1,104 @@
+# programa de bombas de agua para raspberry pi Pico
+from machine import Pin
+from machine import PWM
+import time
+
+# configuracion de pines de entrada y salida
+# definicion de salidas
+BombaPrincipal = Pin(12,Pin.OUT)
+BombaAuxiliar = Pin(13,Pin.OUT)
+
+# definicion de entradas
+BajoBajo = Pin(2, Pin.IN, Pin.PULL_DOWN)
+Bajo = Pin(3, Pin.IN, Pin.PULL_DOWN)
+Alto = Pin(4, Pin.IN, Pin.PULL_DOWN)
+AltoAlto = Pin(5, Pin.IN, Pin.PULL_DOWN)
+BotonInicio = Pin(6, Pin.IN, Pin.PULL_DOWN)
+BotonParo = Pin(7, Pin.IN, Pin.PULL_DOWN)
+
+# configuracion de estados
+EstadoBomba1 = 0
+EstadoBomba2 = 0
+EstadoBotonInicio = 0
+EstadoBotonParo = 0
+EstadoBajoBajo = 0
+EstadoBajo = 0
+EstadoAlto = 0
+EstadoAltoAlto = 0
+Encendido = 1
+Apagado = 0
+espera = 0.1
+EstadoBomba1 = BombaPrincipal
+EstadoBomba2 = BombaAuxiliar
+
+# leer estados de las entradas
+def LeerEstado():
+    global EstadoBajoBajo, EstadoBajo, EstadoAlto, EstadoAltoAlto, EstadoBotonParo
+    EstadoBajoBajo = digitalRead(BajoBajo)
+    EstadoBajo = digitalRead(Bajo)
+    EstadoAlto = digitalRead(Alto)
+    EstadoAltoAlto = digitalRead(AltoAlto)
+    EstadoBotonParo = digitalRead(BotonParo)
+
+# activar bombas
+def ActivarBombas(bomba1, bomba2):
+    global EstadoBomba1, EstadoBomba2
+    digitalWrite(EstadoBomba1, bomba1)
+    time.sleep(espera)
+    digitalWrite(EstadoBomba2, bomba2)
+    time.sleep(espera)
+
+# presionar boton inicio
+def BotonInicio():
+    global EstadoBotonInicio, EstadoBotonParo
+    EstadoBotonParo = digitalRead(BotonParo)
+    if EstadoBotonParo == 1:
+        EstadoBotonInicio = 0
+    else:
+        EstadoBotonInicio = digitalRead(BotonInicio)
+
+# presionar boton de paro
+def BotonParo():
+    global EstadoBomba1, EstadoBomba2
+    if EstadoBomba2 == 13:
+        EstadoBomba1 = 13
+        EstadoBomba2 = 12
+        ActivarBombas(Apagado, Apagado)
+    else:
+        EstadoBomba1 = 12
+        EstadoBomba2 = 13
+        ActivarBombas(Apagado, Apagado)
+
+# bucle principal
+
+BotonInicio()
+
+if EstadoBotonInicio == 1:
+    LeerEstado()
+if EstadoBajoBajo == 1 and EstadoBajo == 0:
+    while EstadoBajoBajo == 1 and EstadoBajo == 0 and EstadoBotonParo == 0:
+        ActivarBombas(Encendido, Encendido)
+        LeerEstado()
+    ActivarBombas(Encendido, Apagado)
+    
+elif EstadoBajo == 1 and EstadoAlto == 0:
+    while EstadoBajo == 1 and EstadoAlto == 0 and EstadoBotonParo == 0:
+        ActivarBombas(Encendido, Apagado)
+        LeerEstado()
+    ActivarBombas(Apagado, Apagado)
+    
+elif EstadoAlto == 1 and EstadoAltoAlto == 0:
+    while EstadoAlto == 1 and EstadoAltoAlto == 0 and EstadoBotonParo == 0:
+        ActivarBombas(Apagado, Apagado)
+        LeerEstado()
+        
+elif EstadoAltoAlto == 1:
+    while EstadoAltoAlto == 1:
+        ActivarBombas(Apagado, Apagado)
+        
+elif digitalRead(BotonParo) == 1:
+    BotonParo()
+    EstadoBotonInicio = 0
+    
+else:
+ActivarBombas(Apagado, Apagado)
